d7d91828cb4602cfb47876cf1e4f70086eb79c1c
linkfqy <linkfqy@gmail.com>
2018-10-25 08:06:32 +0800

    20181025_ 8:06:31

--- z.bat
+++ z.bat
@@ -1,7 +1,6 @@
 set YYYYmmdd=%date:~0,4%%date:~5,2%%date:~8,2%
 set hhmiss=%time:~0,2%:%time:~3,2%:%time:~6,2%
 git add .
-git pull
-git commit -m %YYYYmmdd%_%hhmiss%
+git commit -m "%YYYYmmdd%_%hhmiss%"
 git push
 pause
